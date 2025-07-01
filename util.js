f0174887b5870d822af4fb0f60237f707bab4aef
MightyMushka <155797771+MightyMushka@users.noreply.github.com>
2025-07-01 15:32:48 +0800

    Update util.js

--- util.js
+++ util.js
@@ -1067,6 +1067,21 @@ var util = {
                             new_selection_bounds.max.x = centerX + halfW;
                             new_selection_bounds.min.y = centerY - halfH;
                             new_selection_bounds.max.y = centerY + halfH;
+                        } else if (!util.meta.fit_to_object && util.meta.screen_size && new_selection_bounds) {
+                            // When Fit to Object is off, always use the manual size, never expand
+                            var _w = util.meta.screen_size.width;
+                            var _h = util.meta.screen_size.height;
+                            var dpi = await OBR.scene.grid.getDpi();
+                            var min_width = (_w * dpi);
+                            var min_height = (_h * dpi);
+                            var centerX = (new_selection_bounds.max.x + new_selection_bounds.min.x) / 2;
+                            var centerY = (new_selection_bounds.max.y + new_selection_bounds.min.y) / 2;
+                            var halfW = min_width / 2;
+                            var halfH = min_height / 2;
+                            new_selection_bounds.min.x = centerX - halfW;
+                            new_selection_bounds.max.x = centerX + halfW;
+                            new_selection_bounds.min.y = centerY - halfH;
+                            new_selection_bounds.max.y = centerY + halfH;
                         }
                         await util.setRoomMeta({
                             screen_el: {
@@ -1228,7 +1243,7 @@ var util = {
     },
     updateCurrSelectedScreenEl: async function () {
         var new_selection_bounds = await OBR.scene.items.getItemBounds(util.meta.screen_el.items.arrayOfProp("id"))
-        // If Fit to Object is enabled, enforce minimum screen size and add buffer
+        // Only apply buffer/expand logic if Fit to Object is enabled
         if (util.meta.fit_to_object && util.meta.screen_size && new_selection_bounds) {
             var _w = util.meta.screen_size.width;
             var _h = util.meta.screen_size.height;
@@ -1254,6 +1269,21 @@ var util = {
                 halfW = (Math.max(curr_width, min_width)) / 2;
                 halfH = (Math.max(curr_height, min_height)) / 2;
             }
+            new_selection_bounds.min.x = centerX - halfW;
+            new_selection_bounds.max.x = centerX + halfW;
+            new_selection_bounds.min.y = centerY - halfH;
+            new_selection_bounds.max.y = centerY + halfH;
+        } else if (!util.meta.fit_to_object && util.meta.screen_size && new_selection_bounds) {
+            // When Fit to Object is off, always use the manual size, never expand
+            var _w = util.meta.screen_size.width;
+            var _h = util.meta.screen_size.height;
+            var dpi = await OBR.scene.grid.getDpi();
+            var min_width = (_w * dpi);
+            var min_height = (_h * dpi);
+            var centerX = (new_selection_bounds.max.x + new_selection_bounds.min.x) / 2;
+            var centerY = (new_selection_bounds.max.y + new_selection_bounds.min.y) / 2;
+            var halfW = min_width / 2;
+            var halfH = min_height / 2;
             new_selection_bounds.min.x = centerX - halfW;
             new_selection_bounds.max.x = centerX + halfW;
             new_selection_bounds.min.y = centerY - halfH;
